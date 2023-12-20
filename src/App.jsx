1fd6470b1af8789e9cb33a393279ed9310900513
Ben Chatelain <ben@octop.ad>
2023-12-19 17:14:12 -0700

    ✨ Update App.jsx for meet the react component section

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
 'use strict'
 
-import * as React from 'react'
-
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-}
-
-function getTitle(title) {
-  return title
-}
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div>
-      <h1>Hello {getTitle('React')}</h1>
+  const [count, setCount] = useState(0)
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+  return (
+    <div className='App'>
+      <div>
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src='/vite.svg' className='logo' alt='Vite logo' />
+        </a>
+        <a href='https://reactjs.org' target='_blank'>
+          <img
+            src={reactLogo}
+            className='logo react'
+            alt='React logo'
+          />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className='read-the-docs'>
+        Click on the Vite at React logos to learn more
+      </p>
     </div>
   )
 }
