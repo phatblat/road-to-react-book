6d970adbbd7ec9e44821c8c3b7a257310162141c
Ben Chatelain <ben@octop.ad>
2024-01-23 17:58:13 -0700

    ♻️ Pull loading, error states into reducer

--- src/App.jsx
+++ src/App.jsx
@@ -126,10 +126,27 @@ const List = ({ list, onRemoveItem }) => (
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
-    case 'SET_STORIES':
-      return action.payload
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      }
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      }
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      }
     case 'REMOVE_STORY':
-      return state.filter(
+      return state.data.filter(
         (story) => action.payload.objectID !== story.objectID
       )
     default:
@@ -138,15 +155,19 @@ const storiesReducer = (state, action) => {
 }
 
 const App = () => {
-  // const [stories, setStories] = React.useState(initialStories)
-  const [stories, dispatchStories] = React.useReducer( storiesReducer, [] )
-  const [isLoading, setIsLoading] = React.useState(false)
-  const [isError, setIsError] = React.useState(false)
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer,
+    { data: [], isLoading: false, isError: false }
+  )
   const [searchTerm, setSearchTerm] = useStorageState( 'search', 'React' )
   const handleSearch = (event) => { setSearchTerm(event.target.value) }
 
+  const searchedStories = stories.data.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+
   const handleRemoveStory = (item) => {
-    const newStories = stories.filter(
+    const newStories = stories.data.filter(
       (story) => item.objectID !== story.objectID
     )
     dispatchStories({
@@ -155,27 +176,20 @@ const App = () => {
     })
   }
 
-  const searchedStories = stories.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  )
-
   React.useEffect(() => {
-    setIsLoading(true)
+    dispatchStories({ type: 'STORIES_FETCH_INIT' })
 
     getAsyncStories()
       .then((result) => {
         dispatchStories({
-          type: 'SET_STORIES',
+          type: 'STORIES_FETCH_SUCCESS',
           payload: result.data.stories,
         })
-        setIsLoading(false)
       })
-      .catch(() => setIsError(true))
+      .catch(() =>
+        dispatchStories({ type: 'STORIES_FETCH_FAILURE', })
+      )
   }, [])
-
-  if (isLoading) {
-    return <p>Loading ...</p>
-  }
 
   return (
     <div>
@@ -192,10 +206,10 @@ const App = () => {
 
       <hr />
 
-      {isError && <p>Something went wrong ...</p>}
+      {stories.isError && <p>Something went wrong ...</p>}
 
       {
-        isLoading ? (
+        stories.isLoading ? (
           <p>Loading ...</p>
         ) : (
           <List
